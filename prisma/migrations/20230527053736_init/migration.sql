14dfc352cff351c9999921b2f25fb425da67f045
carloosf <silvaugusto222@gmail.com>
2023-05-27 02:38:04 -0300

    update migrations

--- prisma/migrations/20230527053736_init/migration.sql
+++ prisma/migrations/20230527053736_init/migration.sql
@@ -1,10 +1,13 @@
 -- CreateTable
 CREATE TABLE `tokendb` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `token` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `date` VARCHAR(191) NOT NULL,
     `prioridade` VARCHAR(191) NOT NULL,
+    `status` BOOLEAN NOT NULL,
 
+    UNIQUE INDEX `tokendb_id_key`(`id`),
     UNIQUE INDEX `tokendb_token_key`(`token`),
-    PRIMARY KEY (`token`)
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
