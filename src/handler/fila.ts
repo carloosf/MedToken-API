159ee36ed77af477895271b428a0fa8fb54063e0
carloosf <silvaugusto222@gmail.com>
2023-05-29 04:18:20 -0300

    fila update

--- src/handler/fila.ts
+++ src/handler/fila.ts
@@ -1,56 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { contador } from "./filaControle";
 
 const prisma = new PrismaClient();
 
 export async function Fila() {
-  const spTickets = await prisma.tokendb.findMany({
-    where: { prioridade: 'SP', status: true },
+  const tickets = await prisma.tokendb.findMany({
+    where: { status: true },
+    orderBy: { id: 'asc' },
   });
 
-  const seTickets = await prisma.tokendb.findMany({
-    where: { prioridade: 'SE', status: true },
-  });
-
-  const sgTickets = await prisma.tokendb.findMany({
-    where: { prioridade: 'SG', status: true },
-  });
-
-  if (spTickets.length > 0) {
-    for (const ticket of spTickets) {
-      const randomTimeOffset = Math.floor(Math.random() * 20) + 10;
-      const tempoDeAtendimento = randomTimeOffset;
-      console.log(`${ticket.prioridade} O ticket: ${ticket.token} levou ${tempoDeAtendimento} Minutos`)
-
-      await prisma.tokendb.update({
-        where: { id: ticket.id },
-        data: { status: false },
-      });
-    }
-  } else if (seTickets.length > 0) {
-    for (const ticket of seTickets) {
-      const randomTimeOffset = Math.floor(Math.random() * 1) + 0.1;
-      const tempoDeAtendimento = randomTimeOffset;
-      console.log(`${ticket.prioridade} O ticket: ${ticket.token} levou ${tempoDeAtendimento} Minutos`)
-
-      await prisma.tokendb.update({
-        where: { id: ticket.id },
-        data: { status: false },
-      });
-    }
-  } else if (sgTickets.length > 0) {
-    for (const ticket of sgTickets) {
-      const randomTimeOffset = Math.floor(Math.random() * 17) + 12;
-      const tempoDeAtendimento = randomTimeOffset;
-      console.log(`${ticket.prioridade} O ticket: ${ticket.token} levou ${tempoDeAtendimento} Minutos`)
-
-      await prisma.tokendb.update({
-        where: { id: ticket.id },
-        data: { status: false },
-      });
-    }
+  for (const ticket of tickets) {
+    const { segundosGerais } = await contador(ticket.prioridade);
+    const minutos = Math.floor(segundosGerais / 60);
+    const segundos = segundosGerais % 60;
+    
+    await prisma.tokendb.update({
+      where: { id: ticket.id },
+      data: { status: false },
+    });
   }
 }
-const intervalo = 20000 
-setInterval(async () => {
-  await Fila();
-}, intervalo);
--- src/server.ts
+++ src/server.ts
@@ -1,79 +1,91 @@
-import { PrismaClient } from '@prisma/client'
-import { fastify } from 'fastify'
-import { z } from 'zod'
-import {Fila} from './handler/fila'
-const app = fastify()
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+import { fastify } from 'fastify';
+import { z } from 'zod';
+import { Fila } from './handler/fila';
+
+const app = fastify();
+const prisma = new PrismaClient();
 
 app.get('/', async () => {
-  const tokens = await prisma.tokendb.findMany() 
-  return { tokens }
-})
+  const tokens = await prisma.tokendb.findMany();
+  return { tokens };
+});
 
-app.post('/',async (request, reply) => {
+app.post('/', async (request, reply) => {
   try {
-
     const createTokenSchema = z.object({
       token: z.string(),
       name: z.string(),
       date: z.string(),
       prioridade: z.string(),
-    })
-    
-    const { token, name, date, prioridade } = createTokenSchema.parse(request.body)
-    
+    });
+
+    const { token, name, date, prioridade } = createTokenSchema.parse(request.body);
+
     await prisma.tokendb.create({
-      data : {
+      data: {
         token,
         name,
         date,
         prioridade,
-      }
-    })
-    return reply.status(201).send()
-  } catch(error){
-    console.error(error)
-    return reply.status(500).send({error})
-  }
-  })
+      },
+    });
 
-  app.put('/:id', async (request, reply) => {
-    try {
-      const updateStatusSchema = z.object({
-        id: z.string(),
-      })
-      const { id } = updateStatusSchema.parse(request.params)
-      const idNumber = Number(id);
-        
-      const existingToken = await prisma.tokendb.findUnique({
-        where: { id: idNumber },
-      });
-  
-      if (!existingToken) {
-        return reply.status(404).send({ error: 'Token not found' });
-      }
-  
-      await prisma.tokendb.update({
-        where: { id: idNumber },
-        data: {
-          status: false,
-        },
-      });
-  
-      return reply.status(200).send();
-    } catch (error) {
-      console.error(error);
-      return reply.status(500).send({ error });
+    return reply.status(201).send();
+  } catch (error) {
+    console.error(error);
+    return reply.status(500).send({ error });
+  }
+});
+
+app.put('/:id', async (request, reply) => {
+  try {
+    const updateStatusSchema = z.object({
+      id: z.string(),
+    });
+
+    const { id } = updateStatusSchema.parse(request.params);
+    const idNumber = Number(id);
+
+    const existingToken = await prisma.tokendb.findUnique({
+      where: { id: idNumber },
+    });
+
+    if (!existingToken) {
+      return reply.status(404).send({ error: 'Token not found' });
     }
-  })
+
+    await prisma.tokendb.update({
+      where: { id: idNumber },
+      data: {
+        status: false,
+      },
+    });
+
+    return reply.status(200).send();
+  } catch (error) {
+    console.error(error);
+    return reply.status(500).send({ error });
+  }
+});
 
 app.listen({
-  host: "0.0.0.0",
+  host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }).then(() => {
-  console.log("App Rodando")
+  console.log('App Rodando');
+});
 
-  setInterval(async () => {
+async function iniciarFila() {
+  const getFila = await prisma.tokendb.findMany({
+    where: { status: true },
+  });
+
+  if (getFila.length > 0) {
     await Fila();
-  }, 60000)
-})
+  }
+}
+
+iniciarFila();
+
+setInterval(iniciarFila, 1000);
